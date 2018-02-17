67d9f220395657eec8a3d0c745bac7427c76d214
Matt <matthewcranford314@gmail.com>
2018-02-17 13:53:19 -0800

    Work on algo

--- symmetric-difference.js
+++ symmetric-difference.js
@@ -1,13 +1,56 @@
 
 function sym(args) {
-  var test = arguments[0].reduce(reducer);
-  console.log(test);
-  return args;
+  
+  
+  // hold symetric difference
+  var storedArr = [];
+  
+    
+  // loop through earch arg in args
+  for (var arg = 0; arg< arguments.length; arg++) {
+    console.log(arg,arguments[arg],storedArr);
+    // for each number in arg
+    for (var num =0; num< arguments[arg].length;num++) {
+      console.log(arguments[arg][num]);
+      // if number exists in stored arr
+      if (storedArr.includes(arguments[arg][num])) {
+        // splice it
+        storedArr.splice(storedArr.indexOf(arguments[arg][num]),1);
+      }
+      
+      // else 
+      else {
+        // push it
+        storedArr.push(arguments[arg][num]);
+      }
+        
+    }
+    
+      
+
+  }
+  
+  return storedArr;
+  
+//   var reduced = arguments[i].reduce.call(arguments,function (total,arr) {
+//     console.log(total,arr);
+//     if (!total.includes(arr)) {
+//         total.push(arr);
+//     }
+//     else {
+//       total.pop(arr); 
+//     }
+    
+//     return total;
+//   },[]);
+  
+ 
+
+
+
 }
 
-var reducer = function(accumulator, currentValue) {
-  accumulator = [];
-  return accumulator.push(currentValue);
-};
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+
+// sym([1, 2, 3], [5, 2, 1, 4]);
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]);
