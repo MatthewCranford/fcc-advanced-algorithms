279640237a17106a64f925c058429070c67614ae
Matt <matthewcranford314@gmail.com>
2018-02-25 14:44:12 -0800

    Work on algo

--- no-repeats-please.js
+++ no-repeats-please.js
@@ -1,3 +1,4 @@
+var test = [];
 
 function permAlone(str) {
   
@@ -18,40 +19,44 @@ function permAlone(str) {
   // call findPerm function
   findPerm(strArr, countArr, resultArr, 0);
   // pass strArr, countArr, resultArr, level
-
+  
   console.log("results:", resultArr);
+  return test;
 
 }
 
 function findPerm(strArr, countArr, resultArr, level) {
   
+
   // if level === strArr.length
   if (level === strArr.length) {
     // return resultArr
-    console.log(resultArr);
+    
+    resultArr = resultArr.join('').split(" ");
+//     console.log("resultArr:", resultArr);
+    test.push(resultArr);
     return;
   }
   // for each letter in strArr
   for (var i = 0; i < strArr.length; i++) {
+    
     // if letter !== 0 
-    if (countArr[i] !== 0) {
+    if (countArr[i] === 0) {
+      
+      continue;
+    }
 
-      // add it resultArr
-      resultArr[level]=strArr[i];
+    // add it resultArr
+    resultArr[level] = strArr[i];
+    // countArr --
+    countArr[i] -= 1;
+   
+    // call next level of recursion
+    findPerm(strArr, countArr, resultArr, level + 1);
 
-      // countArr --
-      countArr[i]--;
-
-      // level ++
-      level++;
-
-      // call next level of recursion
-      findPerm(strArr, countArr, resultArr, level);
-
-      // countArr ++
-      countArr[i]++;
-
-    }      
+    // countArr ++
+    countArr[i] += 1;
+      
   }
       
 }
