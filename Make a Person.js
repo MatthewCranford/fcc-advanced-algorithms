f838d42c1dcf9acbcddde2d27a4f3ec4295b4859
Matt <matthewcranford314@gmail.com>
2018-02-27 14:44:35 -0800

    Solve algorithm

--- Make a Person.js
+++ Make a Person.js
@@ -1,45 +1,49 @@
 
 var Person = function(firstAndLast) {
   // Complete the method below and implement the others similarly
+  
+  // return full name
   this.getFullName = function() {
     return firstAndLast;
   };
+  
+  // return first name
   this.getFirstName = function() {
     var first = firstAndLast.split(' ')[0];
     return  first; 
   };
+  
+  // return last name
   this.getLastName = function() {
     var last =  firstAndLast.split(' ')[1];
     return last; 
   };
   
-  
+  // reassign first name
   this.setFirstName = function(first) {
-    
     var nameArr =  firstAndLast.split(' ');
     nameArr[0] = first;
-    narArr.join(" ");
-    console.log(nameArr);
-  
-    return  curFirst; 
+    firstAndLast = nameArr.join(" ");
+    return firstAndLast; 
   };
+  
+  // reassign last name
   this.setLastName = function(last) {
-
-    return; 
-  };
-  this.setFirstAndLast = function(firstAndLast) {
-
-    return; 
+    var nameArr =  firstAndLast.split(' ');
+    nameArr[1] = last;
+    firstAndLast = nameArr.join(" ");
+    return firstAndLast;  
   };
   
-
-  
-
-  
+  // reassign full name
+  this.setFullName = function(newFirstAndLast) {
+    firstAndLast = newFirstAndLast;
+    return firstAndLast; 
+  };
   
     return firstAndLast;
 };
 
 var bob = new Person('Bob Ross');
 bob.getFullName();
-console.log("keys:",Object.keys(bob).length);
+
