0568e3be06c2ced6412e265ab5b398a3944a7eb1
Matt <matthewcranford314@gmail.com>
2018-02-28 13:22:24 -0800

    Work on algo

--- map-the-debris.js
+++ map-the-debris.js
@@ -3,6 +3,16 @@ function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
   
+  var height = arr[0].avgAlt;
+  var a = earthRadius + height;
+  console.log("a=",a);
+  var result = Math.pow(a,3) / GM;
+  console.log("result",result);
+  var resultTwo = Math.sqrt(result);
+  console.log("resultTwo",resultTwo);
+ 
+
+  
   
   return arr;
 }
