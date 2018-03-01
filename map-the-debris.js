997ae0c7c874253222117a8b4e1de1181c091bcf
Matt <matthewcranford314@gmail.com>
2018-03-01 13:58:12 -0800

    Solve algo

--- map-the-debris.js
+++ map-the-debris.js
@@ -3,17 +3,19 @@ function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
   
-  var height = arr[0].avgAlt;
-  var a = earthRadius + height;
-  console.log("a=",a);
-  var result = Math.pow(a,3) / GM;
-  console.log("result",result);
-  var resultTwo = Math.sqrt(result);
-  console.log("resultTwo",resultTwo);
- 
+  // for each object in array
+  for (var item = 0; item < arr.length; item++) {
+    // set height to property value
+    var height = arr[item].avgAlt;
+    
+    // run calc
+    var a = earthRadius + height;
+    var t = Math.round((2 * Math.PI) * (Math.sqrt(Math.pow(a,3) / GM))); 
+    // replace property with orb period
+    delete arr[item].avgAlt;
+    arr[item].orbitalPeriod = t;
+  }
 
-  
-  
   return arr;
 }
 
