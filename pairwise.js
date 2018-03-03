a1d0994f47ad3e753d22c9cd65d80240660bb003
Matt <matthewcranford314@gmail.com>
2018-03-03 14:23:39 -0800

    Work on algo

--- pairwise.js
+++ pairwise.js
@@ -1,6 +1,11 @@
 
 function pairwise(arr, arg) {
   
+  var total = 0;
+  var usedArr = [];
+  
+  console.log("arr", arr,
+              "arg", arg);
 
   // for each index of arr
   arr.forEach(function(item) {
@@ -8,20 +13,43 @@ function pairwise(arr, arg) {
     console.log("each item", item);
     
     // reduce
-    arr.reduce(function(acc,val) {
+    arr.reduce(function(acc,val,index) {
       
-      console.log(
+      
+      
+      if (index > arr.indexOf(acc)) {
+        console.log(
         "acc",acc,
         "val",val,
-        "item",item);
-      
-      if (acc + val === arg) {
-        return 
+        "index",index
+        );
+        
+        if (acc + val === arg) {
+
+          if (!(usedArr.includes(arr.indexOf(acc)) || usedArr.includes(arr.indexOf(val,index)))) {
+            total += (arr.indexOf(acc) + arr.indexOf(val,index));
+            usedArr += arr.indexOf(acc);
+            usedArr += arr.indexOf(val,index);
+            console.log(
+              "acc+val =", acc + val,
+              "index of acc:", arr.indexOf(acc),
+              "index of val:", arr.indexOf(val,arr.indexOf(acc)+1),
+              "total", total
+            ); 
+          }
+          
+       
+
+        }
       }
       
+      
+      return acc;
+      
                
-    return;
+ 
     }, item);
+    
     
   });
   
@@ -30,7 +58,8 @@ function pairwise(arr, arg) {
   
   
   
-  return arg;
+  return total;
 }
 
+// pairwise([1, 1, 1], 2);
 pairwise([1,4,2,3,0,5], 7);
